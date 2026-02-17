491760eb33b258a8419d271ecf3b7d9defc86732
yordypacheco <yspachecom@ufpso.edu.co>
2026-02-17 07:16:50 -0500

    Initial commit

---
+++ Sistema Academico-Arrays/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        scanner sc = new Scanner(System.in);
+
+double notas[] = new double[10];
+
+for (int i=0; i < notas.length; i++{
+
+            System.out.println("ingrese la nota  numero " + i);
+            notas[i]= sc.nextDouble();
+        }
+
+
+        System.out.println("Hello PETRO!");
+
+    }
+}
