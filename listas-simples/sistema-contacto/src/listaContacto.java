cb0ea5d93433ebc32a6d415e2e8d18dc65e82a78
yordy pacheco <yspachecom@ufpso.edu.co>
2026-02-27 07:32:31 -0500

    contactos

---
+++ listas-simples/sistema-contacto/src/listaContacto.java
@@ -0,0 +1,63 @@
+public class listaContacto {
+    private nodo cabeza;
+
+    public listaContacto() {
+        cabeza = null;
+
+    }
+
+    public void insertarFinal(contacto contacto) {
+        nodo nuevo = new nodo(contacto);
+        if (cabeza == null) {
+            cabeza = nuevo;
+        } else {
+            nodo actual = cabeza;
+            while (actual.getSiguiente() != null) {
+                actual = actual.getSiguiente();
+            }
+            actual.setSiguiente(nuevo);
+        }
+    }
+
+    public contacto buscar(String nombre) {
+        nodo actual = cabeza;
+        while (actual != null) {
+            if (actual.getDato().getNombre().equalsIgnoreCase(nombre)) {
+                return actual.getDato();
+            }
+            actual = actual.getSiguiente();
+        }
+        return null;
+    }
+public  boolean eliminar(String nombre){
+        if(cabeza ==null){
+            return false;
+        }
+
+
+        //si es el primero
+    if (cabeza.getDato().getNombre().equalsIgnoreCase(nombre)){
+        cabeza=cabeza.getSiguiente();
+        return true;
+
+    }
+    nodo actual =cabeza;
+
+    while (actual.getSiguiente()!= null){
+        if (actual.getSiguiente().getDato().getNombre().equalsIgnoreCase(nombre)){
+            actual.setSiguiente(actual.getSiguiente().getSiguiente());
+            return true;
+        }
+        actual.setSiguiente(actual.getSiguiente());
+    }
+    return  false;
+}
+
+public  void listar(){
+        nodo actual=cabeza;
+        while (actual !=null){
+            actual.setSiguiente(actual.getSiguiente());
+        }
+}
+
+}
--- listas-simples/sistema-contacto/src/nodo.java
+++ listas-simples/sistema-contacto/src/nodo.java
@@ -1,6 +1,24 @@
 public class nodo {
     private contacto dato;
-    nodo siguiente;
+   private  nodo siguiente;
+
+
+    public contacto getDato() {
+        return dato;
+    }
+
+    public void setDato(contacto dato) {
+        this.dato = dato;
+    }
+
+    public nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+
     public nodo(contacto dato ){
         this.dato= dato;
     }
