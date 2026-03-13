a966a6fcd37bcd8cc7c574a23008db0b6f419763
yordy pacheco <yspachecom@ufpso.edu.co>
2026-03-13 07:41:21 -0500

    ranking de fornite

--- Ranking-de-videojuegos/src/ListaRanking.java
+++ Ranking-de-videojuegos/src/ListaRanking.java
@@ -2,9 +2,75 @@ public class ListaRanking {
 
     private Nodo cabeza;
 
+    public ListaRanking() {
+        cabeza = null;
+
+    }
+
+    public void insertarFinal(Jugador contacto) {
+        Nodo nuevo = new Nodo(contacto);
+        if (cabeza == null) {
+            cabeza = nuevo;
+        } else {
+            Nodo actual = cabeza;
+            while (actual.getSiguiente() != null) {
+                actual = actual.getSiguiente();
+            }
+            actual.setSiguiente(nuevo);
+        }
+    }
+
+    public Jugador buscar(String nombre) {
+        Nodo actual = cabeza;
+        while (actual != null) {
+            if (actual.getDato().getNombre().equalsIgnoreCase(nombre)) {
+                return actual.getDato();
+            }
+            actual = actual.getSiguiente();
+        }
+        return null;
+    }
 
 
-    //insercion
-    //borrar jugador
-    //ordenar jugador
+
+    public  void listar(){
+        Nodo actual=cabeza;
+        while (actual !=null){
+            System.out.println(actual.getDato());
+            actual= actual.getSiguiente();
+
+        }
+    }
+
+    public ListaRanking  lobby (){
+int  max = 100;
+
+
+
+
+
+
+
+
+
+
+       /* Nodo actual = cabeza;
+        int max = actual.getDato().getRanking();
+
+        while ( actual !=null){
+
+            if ( actual.getDato().getRanking()<max){
+max=actual.getDato().getRanking();
+
+
+            }
+actual= actual.getSiguiente();
+
+        }
+
+*/
+return null;
+    }
+
+
 }
--- Ranking-de-videojuegos/src/Main.java
+++ Ranking-de-videojuegos/src/Main.java
@@ -1,6 +1,38 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        ListaRanking lista = new ListaRanking();
+
+
+        lista.insertarFinal(new Jugador("rallador de queso", "batle royale", 2));
+        lista.insertarFinal(new Jugador("juan12p", "batle royale", 8));
+        lista.insertarFinal(new Jugador("rallador de queso dos", "batle royale", 23));
+        lista.insertarFinal(new Jugador("rallador de queso top 1", "batle royale", 99));
+        lista.insertarFinal(new Jugador("sebastian", "batle royale", 1));
+
+
+
+        //registrar puntaje
+        //buscar jugador
+        //ordenar ranking
+
+
+
+
+
+
+
+
+        System.out.println("lista actual \n");
+        lista.listar();
+
+        System.out.println("\nBuscar juan12p :");
+        System.out.println(lista.buscar("juan12p" ));
+
+
+
+        System.out.println("\n lista actual :");
+
+        lista.listar();
     }
 }
