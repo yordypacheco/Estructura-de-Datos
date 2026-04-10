917362947735f19120566edac20f83396c37336d
yordy pacheco <yspachecom@ufpso.edu.co>
2026-04-10 07:51:24 -0500

    listas circulares

---
+++ liastas-circulares/src/model/Contact.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Contact {
+    private String fullName;
+    private  String phoneNumber;
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "fullName='" + fullName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Contact(String fullname, String phoneNumber) {
+        this.fullName = fullname;
+        this.phoneNumber = phoneNumber;
+    }
+}
